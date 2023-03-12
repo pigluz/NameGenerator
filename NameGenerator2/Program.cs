e5f710c7b760ff3b7962969423f376066330097c
pigluz <franciszeklatawiec@gmail.com>
2023-03-12 21:12:18 +0100

    Refactor code

--- NameGenerator2/Program.cs
+++ NameGenerator2/Program.cs
@@ -4,71 +4,58 @@ string[] surnames = { "Smith", "Brown", "Wilson", "Thomson", "Robertson", "Campb
 
 Random rand = new Random();
 
-    Console.WriteLine("Name generator");
-    Thread.Sleep(1000);
+Console.WriteLine("Name generator");
+Thread.Sleep(1000);
 
-outerLoop:
+
+while (true)
+{
+    Console.WriteLine("\nChoose gender (Male/Female):");
+    string gender_choice = Console.ReadLine().ToLower();
+
     while (true)
     {
-        Console.WriteLine("\nChoose gender (Male/Female):");
-    string gender_choice = Console.ReadLine();
-
-    innerLoop:
-    while(true)
-    {
-        int RandomIndex_maleNames = rand.Next(maleNames.Length);
-        string Random_maleNames = maleNames[RandomIndex_maleNames];
-
-        int RandomIndex_femaleName = rand.Next(femaleNames.Length);
-        string Random_femaleNames = femaleNames[RandomIndex_femaleName];
-
         int RandomIndex_surnames = rand.Next(surnames.Length);
         string Random_surnames = surnames[RandomIndex_surnames];
 
         int RandomAge = rand.Next(18, 30);
 
-        if (gender_choice == "Male")
+        if (gender_choice == "male")
         {
+            int RandomIndex_maleNames = rand.Next(maleNames.Length);
+            string Random_maleNames = maleNames[RandomIndex_maleNames];
+
             Console.WriteLine
                 ("\nName: " + Random_maleNames + "\n" +
                 "Surname: " + Random_surnames + "\n" +
                 "Age: " + RandomAge);
-            Thread.Sleep(1000);
-            Console.WriteLine("\nDo you want to generate again?");
-            string anotherGenerate_choice = Console.ReadLine();
-
-            if (anotherGenerate_choice == "Yes")
-            {
-                goto innerLoop;
-            } else
-            {
-                return;
-                //Environment.Exit(0);
-            }
         }
-        else if (gender_choice == "Female")
+        else if (gender_choice == "female")
         {
+            int RandomIndex_femaleName = rand.Next(femaleNames.Length);
+            string Random_femaleNames = femaleNames[RandomIndex_femaleName];
+
             Console.WriteLine
                 ("\nName: " + Random_femaleNames + "\n" +
                 "Surname: " + Random_surnames + "\n" +
                 "Age: " + RandomAge);
-            Thread.Sleep(1000);
-            Console.WriteLine("\nDo you want to generate again?");
-            string anotherGenerate_choice = Console.ReadLine();
-
-            if (anotherGenerate_choice == "Yes")
-            {
-                goto innerLoop;
-            } else
-            {
-                return;
-                //Environment.Exit(0);
-            }
-
-        } else
+        }
+        else
         {
             Console.WriteLine("\nIncorrect gender.");
-            goto outerLoop;
-        }  
+            break;
+        }
+        Thread.Sleep(1000);
+
+
+        Console.WriteLine("\nDo you want to generate again?");
+        string generateInput = Console.ReadLine().ToLower();
+
+        if (generateInput == "no")
+        {
+            break;
+        }
     }
 }
+
+
